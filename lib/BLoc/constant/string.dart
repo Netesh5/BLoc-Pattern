09118bb853a8e226b618777960805f650385a056
netesh paudel <netesh.paudel@gmail.com>
2021-09-25 22:23:19 +0545

    api

---
+++ lib/BLoc/constant/string.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+class Strings {
+  // ignore: non_constant_identifier_names
+  static var News_url =
+      "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=d7cfb7a1cf134766aadaf2ad5538355b";
+}
--- lib/apimanager.dart
+++ lib/apimanager.dart
@@ -1,3 +1,14 @@
+import 'package:blocstateapp/BLoc/constant/string.dart';
+import 'package:http/http.dart' as http;
+
+// ignore: camel_case_types
 class API_manger {
-  void getnews() {}
+  void getnews() async {
+    var client = http.Client();
+    // ignore: unused_local_variable
+    var response = await client.get(Uri.parse(Strings.News_url));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+    }
+  }
 }
